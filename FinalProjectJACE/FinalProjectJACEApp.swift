e80580e20d3cf0b30a0664d2203664ec5a5889bc
Chioma <chiomaigwe494@gmail.com>
2023-08-01 09:36:28 +0100

    Initial Commit

---
+++ FinalProjectJACE/FinalProjectJACEApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectJACEApp.swift
+//  FinalProjectJACE
+//
+//  Created by scholar on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectJACEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
