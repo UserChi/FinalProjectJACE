9c478a3b62dbaffadf8dc30554729eca14512352
Angelique <angelique@tcpglobalsolutions.com>
2023-08-03 10:23:08 +0100

    Home Page Button Added

---
+++ FinalProjectJACE/Home Page/HomePage.swift
@@ -0,0 +1,20 @@
+//
+//  HomePage.swift
+//  FinalProjectJACE
+//
+//  Created by scholar on 03/08/2023.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text("Transition from home page")
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
